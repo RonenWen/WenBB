28c8884176b33dda5d31b1662de33b5ad0c2936e
RonenWen <wjqronen@gmail.com>
2017-07-08 19:48:15 +0800

    1

---
+++ 未命名1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<cstdlib>
+#include<cmath>
+using namespace std;
+int main()
+{
+	int a, b;
+	char x;
+	x=getchar();
+	a = x;
+	if (a % 2 == 0)
+		cout << "NO";
+	else cout << "Yes";
+	system("pause");
+	return 0;
+}
