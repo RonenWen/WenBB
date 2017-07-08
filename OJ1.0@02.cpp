b638dd1e1512a46592a4df02294d9b29d9ab9380
RonenWen <wjqronen@gmail.com>
2017-07-08 22:54:37 +0800

    OJ的题

---
+++ OJ1.0@02.cpp
@@ -0,0 +1,15 @@
+//输出第二个整数
+//c++
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+int main()
+{
+  int a,b,c;
+  cin>>a;
+  cin>>b;
+  cin>>c;
+  cout<<b<<endl;
+  system("pause");
+  return 0;
+}
