97507e43a667b7010b0d9d0c68cfe63f34e595e1
RonenWen <wjqronen@gmail.com>
2017-07-08 22:30:30 +0800

    OJ1.0-01

---
+++ OJ1.0@01.cpp
@@ -0,0 +1,16 @@
+//Hello world!
+//pascal
+begin  
+writeln('Hello, World!')  
+end.
+
+//C++
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+int main()
+{
+    cout<<"Hello, World!"<<endl;
+    system("pause");
+    return 0;
+}
